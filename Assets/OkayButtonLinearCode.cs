88db0cf3f674dc29540def945ad83383adc90391
aday913 <45890246+aday913@users.noreply.github.com>
2018-12-14 21:44:54 -0700

    Initial commit

---
+++ Assets/OkayButtonLinearCode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OkayButtonLinearCode : MonoBehaviour {
+    public int TargetScene;
+    public void NextSceneOnClick()
+    {
+        SceneManager.LoadScene(TargetScene);
+    }
+}
---
+++ Assets/TextInputHandle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInputHandle : MonoBehaviour {
+    //public string text;
+    public void TextInput(string input)
+    {
+        GameObject.FindGameObjectWithTag("Manager").GetComponent<Settings>().userInput = input;
+       // text = input;
+    }
+}
